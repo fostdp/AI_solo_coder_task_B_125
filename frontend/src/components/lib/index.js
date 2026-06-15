4793f21e006bbff471125674ebfb9a94d6a16d1d
fostdp <129711106+fostdp@users.noreply.github.com>
2026-06-16 07:24:54 +0800

    Update project

---
+++ frontend/src/components/lib/index.js
@@ -0,0 +1,7 @@
+/**
+ * 前端独立模块索引
+ * 对外暴露可独立使用的模块
+ */
+
+export { Pagoda3DRenderer, default as pagoda3d } from './pagoda_3d.js'
+export { HealthPanel, StructuralHealthAssessor, default as healthPanel } from './health_panel.js'
